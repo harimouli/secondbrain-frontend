466cf06b06f6c9ee009a40296e6e90ad732ea7ca
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-06-06 15:20:52 +0530

    flies moved

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
           
           <Route path = "/signin" element = {<Signin/>}/>
           <Route path = "/signup" element = {<Signup/>}/>
-          <Route path = "/dashboard" element = {<Dashboard />}/>
+          <Route path = "/dashboard" element = {<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
   )
