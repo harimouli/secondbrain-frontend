72eea45b910861973258d453134795eb909ce9a4
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-07-04 17:53:06 +0530

    updated addcontent

--- src/components/CreateContentModal.tsx
+++ src/components/CreateContentModal.tsx
@@ -6,6 +6,7 @@ import { Button } from "./Button";
 import { Input } from "./Input";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
+import { InputLabel } from "./InputLabel";
 
 
 interface CreateContentModalProps {
@@ -54,25 +55,38 @@ export const CreateContentModal = ({ open, onClose }: CreateContentModalProps) =
             {open && (
                 <div className="w-screen h-screen fixed bg-black/50 flex justify-center items-center top-0 left-0" >
                     <div className = "flex flex-col justify-center bg-white opacity-100 rounded-lg shadow-xl">
-                        <span className = "bg-white rounded p-4">
+                        <span className = "bg-white rounded p-8 ">
                             <div className = "flex justify-end ">
                                 <div onClick = {() => onClose(false)}>
                                     {<CrossIcon  size = {"lg"}/>}
                                 </div>  
                             </div>
-                            <div className = "flex flex-col">
-                                <Input reference = {titleRef} type= {"text"} placeholder=  {"Title"}/>
-                                <Input reference={linkRef} type = {"text"} placeholder={"Link"} />
+                            <div className = "flex flex-col items-center">
+                                <div className = "p-2 flex flex-col">
+                                     <InputLabel labelText="Enter the Title:" htmlfor = "title"/>
+                                     <div className = "mt-1">
+                                                  <Input id = "title" width = "w-83" reference = {titleRef} type= {"text"} placeholder=  {"Title"}/>
+                                     </div>
+        
+                                </div>
+                                <div className = "p-2 flex flex-col">
+                                    <InputLabel labelText="Enter the Link:" htmlfor = "link"/>
+                                    <div className = "mt-1">
+                                         <Input width = "w-83" reference={linkRef} type = {"text"} placeholder={"Link"} />
+                                    </div>
+                                    
+                                </div>
+                               
                                 <div className = "flex flex-wrap gap-3 items-center justify-center">
                                         <Button onClick={()=> {
                                             setType(ContentType.Youtube)
                                         }}  variant= {type == ContentType.Youtube ? "primary": "secondary"} 
-                                        text = "youtube" size="sm"/>
+                                        text = "youtube" size="md"/>
                                         <Button
                                         onClick={() => {
                                             setType(ContentType.Twitter)
                                         }}
-                                         variant= {type == ContentType.Twitter ? "primary": "secondary"} text="twitter" size = "sm"/>
+                                         variant= {type == ContentType.Twitter ? "primary": "secondary"} text="twitter" size = "md"/>
                                           
                                 </div>
                             </div>
