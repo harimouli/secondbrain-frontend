67551d53f3390277e39e47ea72bbbd34110d954f
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2026-01-02 00:03:09 +0530

    fix:data fetching axios updated

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -75,142 +75,143 @@ export function Dashboard() {
   const isNoContent: number = filteredContent.length;
 
   return (
-    <div>
-      {isSidebarOpen && (
-        <Sidebar
-          isSidebarOpen={isSidebarOpen}
-          setSidebar={setSidebar}
-          activeBar={activeBar}
-          setActiveBar={setActiveBar}
-        />
-      )}
+    <main>
+      <div>
+        {isSidebarOpen && (
+          <Sidebar
+            isSidebarOpen={isSidebarOpen}
+            setSidebar={setSidebar}
+            activeBar={activeBar}
+            setActiveBar={setActiveBar}
+          />
+        )}
 
-      <div
-        className={`min-h-screen bg-white ${isSidebarOpen && isDesktop ? "ml-72" : "w-[100%]"}`}
-      >
-        {/*menu  when sidebar is closed*/}
+        <div
+          className={`min-h-screen bg-white ${isSidebarOpen && isDesktop ? "ml-72" : "w-[100%]"}`}
+        >
+          {/*menu  when sidebar is closed*/}
 
-        <ShareModel
-          open={openShareModel}
-          onClose={() => setOpenShareModel(false)}
-        />
-        <CreateContentModal
-          refreshContent={refreshContent}
-          open={modelOpen}
-          onClose={setModelOpen}
-        />
-        {/*Header*/}
-        <nav className="hidden lg:flex  border-b border-b-slate-300  items-center">
-          <div className={`flex w-full`}>
-            {!isSidebarOpen && (
-              <div
-                className="  p-5 text-slate-400"
+          <ShareModel
+            open={openShareModel}
+            onClose={() => setOpenShareModel(false)}
+          />
+          <CreateContentModal
+            refreshContent={refreshContent}
+            open={modelOpen}
+            onClose={setModelOpen}
+          />
+          {/*Header*/}
+          <nav className="hidden lg:flex  border-b border-b-slate-300  items-center">
+            <div className={`flex w-full`}>
+              {!isSidebarOpen && (
+                <div
+                  className="  p-5 text-slate-400"
+                  onClick={() => {
+                    setSidebar(!isSidebarOpen);
+                  }}
+                >
+                  <IoMenuSharp size="30" />
+                </div>
+              )}
+              <div className="md:flex items-center max-w-xl gap-4 w-[50%] p-3">
+                <div className="flex items-center w-full border rounded-md outline-none  border-slate-300 px-1">
+                  <IoIosSearch className="text-slate-400" size="25" />
+                  <input
+                    type="search"
+                    placeholder="search your content"
+                    className="border-none  text-base text-slate-600  rounded-md p-2 w-[100%]  md:text-md  outline-none"
+                  />
+                </div>
+              </div>
+
+              <div className="flex md:flex items-center justify-end gap-6 p-4 ml-auto">
+                <Button
+                  onClick={() => setModelOpen(true)}
+                  startIcon={<Plus size={"20px"} />}
+                  size={ButtonSize.Medium}
+                  variant={ButtonVariant.Primary}
+                  text={isDesktop || isTablet ? "Add Link" : ""}
+                ></Button>
+                <Button
+                  onClick={() => setOpenShareModel(true)}
+                  startIcon={<Share2 size={"20px"} />}
+                  size={ButtonSize.Medium}
+                  variant={ButtonVariant.Secondary}
+                  text={isDesktop || isTablet ? "Share" : ""}
+                ></Button>
+              </div>
+            </div>
+          </nav>
+
+          <nav className=" lg:hidden w-full flex items-center justify-between   p-3  border-b border-b-slate-300">
+            <div className="w-[10%]">
+              <Menu
+                size="35"
                 onClick={() => {
                   setSidebar(!isSidebarOpen);
                 }}
-              >
-                <IoMenuSharp size="30" />
-              </div>
-            )}
-            <div className="md:flex items-center max-w-xl gap-4 w-[50%] p-3">
-              <div className="flex items-center w-full border rounded-md outline-none  border-slate-300 px-1">
-                <IoIosSearch className="text-slate-400" size="25" />
-                <input
-                  type="search"
-                  placeholder="search your content"
-                  className="border-none  text-base text-slate-600  rounded-md p-2 w-[100%]  md:text-md  outline-none"
-                />
-              </div>
-            </div>
-
-            <div className="flex md:flex items-center justify-end gap-6 p-4 ml-auto">
-              <Button
-                onClick={() => setModelOpen(true)}
-                startIcon={<Plus size={"20px"} />}
-                size={ButtonSize.Medium}
-                variant={ButtonVariant.Primary}
-                text={isDesktop || isTablet ? "Add Link" : ""}
-              ></Button>
-              <Button
-                onClick={() => setOpenShareModel(true)}
-                startIcon={<Share2 size={"20px"} />}
-                size={ButtonSize.Medium}
-                variant={ButtonVariant.Secondary}
-                text={isDesktop || isTablet ? "Share" : ""}
-              ></Button>
-            </div>
-          </div>
-        </nav>
-
-        <nav className=" lg:hidden w-full flex items-center justify-between   p-3  border-b border-b-slate-300">
-          <div className="w-[10%]">
-            <Menu
-              size="35"
-              onClick={() => {
-                setSidebar(!isSidebarOpen);
-              }}
-              className="text-slate-400"
-            />
-          </div>
-          <div className="flex items-center gap-3 w-[90%] justify-end">
-            <div className="relative flex items-center  w-[75%] border border-slate-300 rounded-md px-2 py-2">
-              <IoIosSearch size="20" className="text-slate-300" />
-              <input
-                type="search"
-                placeholder="Search..."
-                className="border-none  text-base  rounded-md  outline-none"
+                className="text-slate-400"
               />
             </div>
+            <div className="flex items-center gap-3 w-[90%] justify-end">
+              <div className="relative flex items-center  w-[75%] border border-slate-300 rounded-md px-2 py-2">
+                <IoIosSearch size="20" className="text-slate-300" />
+                <input
+                  type="search"
+                  placeholder="Search..."
+                  className="border-none  text-base  rounded-md  outline-none"
+                />
+              </div>
 
-            <div className="flex items-center gap-3 w-[25%] justify-end transition-all ease-in-out duration-1000">
-              <Button
-                onClick={() => setModelOpen(true)}
-                startIcon={<Plus size={"20px"} />}
-                size={ButtonSize.Small}
-                variant={ButtonVariant.Primary}
-                text={isDesktop || isTablet ? "Add Link" : ""}
-              ></Button>
-              <Button
-                onClick={() => setOpenShareModel(true)}
-                startIcon={<Share2 size={"18px"} />}
-                size={ButtonSize.Small}
-                variant={ButtonVariant.Secondary}
-                text={isDesktop || isTablet ? "Share" : ""}
-              ></Button>
+              <div className="flex items-center gap-3 w-[25%] justify-end transition-all ease-in-out duration-1000">
+                <Button
+                  onClick={() => setModelOpen(true)}
+                  startIcon={<Plus size={"20px"} />}
+                  size={ButtonSize.Small}
+                  variant={ButtonVariant.Primary}
+                  text={isDesktop || isTablet ? "Add Link" : ""}
+                ></Button>
+                <Button
+                  onClick={() => setOpenShareModel(true)}
+                  startIcon={<Share2 size={"18px"} />}
+                  size={ButtonSize.Small}
+                  variant={ButtonVariant.Secondary}
+                  text={isDesktop || isTablet ? "Share" : ""}
+                ></Button>
+              </div>
             </div>
-          </div>
-        </nav>
+          </nav>
 
-        {/*Rending the Links of user*/}
-        {isLoading ? (
-          <LoadingView />
-        ) : isNoContent === 0 ? (
-          <NoContentView />
-        ) : (
-          <div className="flex flex-col items-center md:flex-wrap md:flex-row md:items-start gap-3 mt-4 p-3">
-            {filteredContent.map(
-              ({ title, type, link, _id }: ContentType) => (
-                console.log(typeof _id),
-                (
+          {/*Rending the Links of user*/}
+          {isLoading ? (
+            <LoadingView />
+          ) : isNoContent === 0 ? (
+            <NoContentView />
+          ) : (
+            <div className="flex flex-col items-center md:flex-wrap md:flex-row md:items-start gap-3 mt-4 p-3">
+              {filteredContent.map(
+                ({ title, type, link, _id, share }: ContentType) => (
                   <Card
                     key={_id}
                     title={title}
                     link={link}
+                    contentId={_id}
                     type={
                       type as "document" | "youtube" | "twitter" | "linkedin"
                     }
                     refreshContent={refreshContent}
+                    share={share}
                   />
-                )
-              ),
-            )}
+                ),
+              )}
+            </div>
+          )}
+
+          <div className="hidden fixed z-50 bottom-20 right-5 gap-1 ">
+            <button className="text-2xl text-amber-300">Theme</button>
           </div>
-        )}
-        {/*<embed src="https://100xdevs.com/" className="w-[500px] h-[300px]"/>*/}
-        <div className="hidden fixed z-50 bottom-20 right-5 gap-1 ">
-          <button className="text-2xl text-amber-300">Theme</button>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
