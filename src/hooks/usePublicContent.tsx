0d0472aacd16593e557a4d5574d37aae2d338b90
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2026-01-23 12:05:40 +0530

    feat: added hook for shared comtent

---
+++ src/hooks/usePublicContent.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../config";
+
+export const usePublicContent = (hash: string) => {
+  const [publicData, setPublicData] = useState<object | null>(null);
+
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    refreshPublicData();
+  }, []);
+
+  const refreshPublicData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `${BACKEND_URL}/api/v1/public-content/${hash}`,
+      );
+      setPublicData(response.data);
+    } catch (error) {
+      console.error("Error fetching public content:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return [publicData, refreshPublicData, loading] as const;
+};
