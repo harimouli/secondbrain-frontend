5931f114a1fdd760d89f7000d52a6d8637a8e0ad
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2026-01-23 12:06:50 +0530

    feat: public share route addded

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,46 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Dashboard } from "./pages/dashboard";
-
+import { NotFound } from "./components/NotFound";
 import { ProtectedRoute } from "./ProtectedRoute";
-import  {AuthMain} from "./components/AuthMain";
-import  Landing  from "./components/Landing";
-import { ToastContainer } from 'react-toastify';
+import { AuthMain } from "./components/AuthMain";
+import Landing from "./components/Landing";
+import { ToastContainer } from "react-toastify";
+import { PublicContent } from "./components/PublicContent";
 import "react-toastify/dist/ReactToastify.css";
 
 import { Profile } from "./components/Profile";
 const App = () => {
   return (
-    <BrowserRouter>        
-       <ToastContainer position="top-right" autoClose={3000}  toastStyle={{maxWidth: "w-[100px] md:w-[300px]"}} />
+    <BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        toastStyle={{ maxWidth: "w-[100px] md:w-[300px]" }}
+      />
       <Routes>
-          <Route path = "/" element = {<Landing/>}/>
-          <Route path = "/auth" element = {<AuthMain/>}/>
-          <Route  path = "/dashboard" element = {
+        <Route path="/" element={<Landing />} />
+        <Route path="/auth" element={<AuthMain />} />
+        <Route
+          path="/dashboard"
+          element={
             <ProtectedRoute>
-                <Dashboard/>
-            </ProtectedRoute>        
-            }/>
-            <Route path = "/profile" element = {
-              <ProtectedRoute>
-                  <Profile/>
-              </ProtectedRoute> 
-            }/>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="mind/:hash" element={<PublicContent />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
